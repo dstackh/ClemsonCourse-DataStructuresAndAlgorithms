f35a1e98e190a9b4816c62ff17d8791840f16e62
Smith Stackhouse <dstackh@g.clemson.edu>
2020-12-13 14:06:16 -0500

    Create main.cpp

---
+++ LinkedListBasedStacksAndQueues/main.cpp
@@ -0,0 +1,20 @@
+/*
+Smith Stackhouse
+1/31/2020
+Lab Section: 003
+*/
+
+
+#include <iostream>
+#include <string>
+#include "List.h"
+using namespace std;
+
+int main()
+{
+    List <int> listInt;
+    List <string> listStr;
+
+
+    return 0;
+}
